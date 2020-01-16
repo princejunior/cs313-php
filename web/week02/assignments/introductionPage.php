5660391e93e50597d8a1585b6c32afa27f3c9262
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-01-16 13:02:51 -0700

    start of week02 assignment

---
+++ web/week02/assignments/introductionPage.php
@@ -0,0 +1,16 @@
+<html lang="en">
+    <head>
+    </head>
+<body>
+    <div> 
+        <?php
+        $name= "Elijah Elliott";
+        $aboutME ="I am studying in Software Engineer. 
+        Home is in Illinois and I do not miss the place. ";
+           echo "<img src=''>";
+           echo "<header><h1>" . $name . "</h1></header>";  
+           echo "<div><p>" . $aboutME . "</p></div>";
+        ?>    
+    </div>
+</body>
+</html>
