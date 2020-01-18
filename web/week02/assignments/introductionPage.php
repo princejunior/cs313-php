6b16b4fe4639fb8e0a7d320ef3602be9de40b0d0
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-01-18 12:01:37 -0700

    applied buttons

--- web/week02/assignments/introductionPage.php
+++ web/week02/assignments/introductionPage.php
@@ -28,12 +28,17 @@
     <div class="col-sm-4">
       <h3>About Me</h3>
       <p><?php echo $aboutME;?></p>
+      <button type="button" class="btn" href="homePage.html">Go Back</button>
+     <a href="homePage.html">Go to Home Page</a>;
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
     <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        <div class="thumbnail">
+            <a href="introductionPage.php" target="_blank">
+                <img class="img-thumbnail" src="<?php echo $image;?>" 
+                alt="Family" style="max-width:100%">
+            </a>
+        </div>
     </div>
     <div class="col-sm-4">
       <h3>Column 3</h3>        
@@ -42,11 +47,60 @@
     </div>
   </div>
 </div>
+<script>
+    function changeInfo(value){
+        if(value == "me"){
+            document.getElementByID("aboutME").style.visibility = visible;
+            document.getElementByID("education").style.visibility = hidden;
+            document.getElementByID("goals").style.visibility = hidden;
+        }
+        else if(value == "education"){
+            document.getElementByID("aboutME").style.visibility = hidden;
+            document.getElementByID("education").style.visibility = visible;
+            document.getElementByID("goals").style.visibility = hidden;
+        }
+        else if(value == "goals"){
+            document.getElementByID("aboutME").style.visibility = hidden;
+            document.getElementByID("education").style.visibility = hidden;
+            document.getElementByID("goals").style.visibility = visible;
+        }
+    } 
+</script>
 
-
-    <div> 
-    <a href="homePage.html">Go to Home Page</a>;
-        
+<div class="container">
+<div class="btn-group">
+  <button type="button" class="btn btn-primary" 
+  onclick="changeInfo(this)" value="me">About Me </button>
+  <button type="button" class="btn btn-primary" 
+  onclick="changeInfo(this)" value="education">Education</button>
+  <button type="button" class="btn btn-primary" 
+  onclick="changeInfo(this)" value="goals">Goals</button>
+</div>
+  <div class="row">
+    <div class="col-sm-4" id="aboutMe">
+      <h3>About Me</h3>
+      <p><?php echo $aboutME;?></p>
+      <button type="button" class="btn" href="homePage.html">Go Back</button>
+     <a href="homePage.html">Go to Home Page</a>;
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
+    <div class="col-sm-4" id="education">
+        <div class="thumbnail">
+            <a href="introductionPage.php" target="_blank">
+                <img class="img-thumbnail" src="<?php echo $image;?>" 
+                alt="Family" style="max-width:100%">
+            </a>
+        </div>
+    </div>
+    <div class="col-sm-4" id="goals">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+
+        
+   
 </body>
 </html>
