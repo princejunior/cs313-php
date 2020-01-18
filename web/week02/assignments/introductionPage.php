383ae5ce3b18a70f837a503f276feadefc5e072d
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-01-18 11:41:20 -0700

    applied bs

--- web/week02/assignments/introductionPage.php
+++ web/week02/assignments/introductionPage.php
@@ -1,17 +1,52 @@
 <html lang="en">
     <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Found in https://www.w3schools.com/bootstrap/bootstrap_get_started.asp -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
 <body>
-    <div> 
-    <a href="homePage.html">Go to Home Page</a>;
-        <?php
-        $name= "Elijah Elliott";
+<?php
+        $name= "Elijah Elliott's Page";
+        $family = " I am one of four childeren being the second youngest.";
         $aboutME ="I am studying in Software Engineer. 
         Home is in Illinois and I do not miss the place. ";
-           echo "<img src=''>";
-           echo "<header><h1>" . $name . "</h1></header>";  
-           echo "<div><p>" . $aboutME . "</p></div>";
-        ?>    
+        $image = "<img src='/photo/IMG_5690.jpeg'";
+?>    
+<div class="jumbotron text-center">
+  <h1><?php echo $name;?></h1>
+  <p><?php echo $family;?></p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>About Me</h3>
+      <p><?php echo $aboutME;?></p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+
+
+    <div> 
+    <a href="homePage.html">Go to Home Page</a>;
+        
     </div>
 </body>
 </html>
