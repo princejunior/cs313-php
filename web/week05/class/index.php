5cd3e508fe837cd473e09f424d426cb87fbc38b9
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-02-04 15:48:48 -0700

    class work and database 2

--- web/week05/class/index.php
+++ web/week05/class/index.php
@@ -6,6 +6,10 @@
     $family_members->execute();
 
     while($fRow = $family_members->fetch(PDO::FETCH_ASSOC)){
-        
+        $first_name = $fRow['first_name'];
+        $last_name = $fRow['last_name'];
+        $relationship_id = $fRow['relationship_id'];
+
+        echo "<p>$first_name $last_name is my $relationship_id</p>";
     }
 ?>
