e0a959ed1be5ed9a032b13e1a0324e13186e7ef3
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-02-06 16:29:16 -0700

    added thursaday week 05

--- web/week05/class/w5ThursdayClassActivity/index.php
+++ web/week05/class/w5ThursdayClassActivity/index.php
@@ -1,3 +1,57 @@
 <?php 
+function get_db(){
+       $db = NULL;
+    try
+{
+    // access url by keyword 
+  $dbUrl = getenv('DATABASE_URL');
 
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+return $db;
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<?php 
+    require "dbConnect.php";
+    $db = get_db();
+    $event_participnat_Data = NULL;
+?>
+<?php
+    $statement = $db->prepare("SELECT * FROM w5_event");
+    $statement->execute();
+    // Go through each result
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+        // The variable "row" now holds the complete record for that
+        // row, and we can access the different values based on their
+        // name
+        $name = $row['name'];
+        $picture_img_url = $row['img'];
+        $description = $row['description'];
+        $id = $row['id'];?>
+        
+        <img src="<?php echo $profile_img_url;?>">
+        </body>
+        <?php 
+    }
+    ?>
+</html>
--- web/week05/class/w5ThursdayClassActivity/query.sql
+++ web/week05/class/w5ThursdayClassActivity/query.sql
@@ -1,28 +1,54 @@
 \echo '2a. How many events are there?'
 ---------------- Your code here ----------------
-
--- \echo '2b. How many participants are there?'
+SELECT count(id) FROM w5_event;
+\echo '2b. How many participants are there?'
 ---------------- Your code here ----------------
-
--- \echo '3a. What is the third event when sorted alphabetically (by name)? '
+SELECT count(id) FROM w5_participant;
+\echo '3a. What is the third event when sorted alphabetically (by name)? '
 ---------------- Your code here ----------------
-
--- \echo '3b. What is the third event when sorted by ID? '
+-- SKIP THE FIRST 2 ROWS AND SELECT THE FIRST ROW
+SELECT name FROM w5_event ORDER BY name LIMIT 1 OFFSET 2; 
+\echo '3b. What is the third event when sorted by ID? '
 ---------------- Your code here ----------------
-
--- \echo '4a. List the names alphabetically of all the participants in the ''Toughman Utah'' competition'
+SELECT id FROM w5_event ORDER BY name LIMIT 1 OFFSET 2; 
+\echo '4a. List the names alphabetically of all the participants in the ''Toughman Utah'' competition'
 ---------------- Your code here ----------------
+select p.name 
+FROM w5_participant AS p
+inner join w5_event_participant as ep ON ep.participant_id = p.id
+inner JOIN w5_event AS e ON ep.event_id = e.id
+WHERE e.name = 'Toughman Utah'
+ORDER BY p.name;
+-- SELECT id from w5_event_participant
+-- left join w5_event
+-- on w5_event_participant.event_id = w5_event.id
+-- right join w5_participant
+-- on w5_event_participant.participant_id = w5_participant.id;
 
--- \echo '4b. List the names (sorted by id) of all the participants in the ''Kauai Marathon'' competition'
+\echo '4b. List the names (sorted by id) of all the participants in the ''Kauai Marathon'' competition'
 ---------------- Your code here ----------------
-
+select p.name 
+FROM w5_participant AS p
+inner join w5_event_participant as ep ON ep.participant_id = p.id
+inner JOIN w5_event AS e ON ep.event_id = e.id
+WHERE e.name = 'Kauai Marathon'
+ORDER BY p.id;
 -- \echo '5a. How many competitions has ''Black Panther'' competed in?'
 ---------------- Your code here ----------------
-
--- \echo '5b. How can you verify that your count is correct?'  
+SELECT count(*)
+from (SELECT 
+        FROM w5_participant AS p
+        inner join w5_event_participant AS ep ON ep.participant_id = p.id
+        inner join w5_event AS e ON ep.event_id = e.id
+        WHERE p.name = 'Black Panther') AS ep_events;
+\echo '5b. How can you verify that your count is correct?'  
 ---------------- Your code here ----------------
-
--- \echo '5c. Who has competed in the most competitions? How many have they competed in?'
+SELECT e.name AS event_name, p.name as participant_id
+FROM w5_participant AS p
+        inner join w5_event_participant AS ep ON ep.participant_id = p.id
+        inner join w5_event AS e ON ep.event_id = e.id
+        WHERE p.name = 'Black Panther';
+\echo '5c. Who has competed in the most competitions? How many have they competed in?'
 ---------------- Your code here ----------------
 
 -- \echo '5d. Who has competed in the least competitions? How many have they competed in?'
