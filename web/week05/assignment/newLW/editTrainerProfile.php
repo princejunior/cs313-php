b8ee7889aeafd204ed576f25e340b3af57f8e9ec
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-02-12 04:24:25 -0700

    added icons to editTrainerProfile/page.php

--- web/week05/assignment/newLW/editTrainerProfile.php
+++ web/week05/assignment/newLW/editTrainerProfile.php
@@ -181,7 +181,7 @@
           <div class="col-md-12 probootstrap-relative">
             <div>
               <h2 class="probootstrap-heading mt0 mb50">Programs (For Beginners)</h2>
-              <a href='editTrainerProfile.php?id=<?php echo $id?>' class="probootstrap-owl-navigation absolute left"><button><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>Add Programs</button></a>            
+              <a href='editTrainerProfile.php?id=<?php echo $id?>' class="probootstrap-owl-navigation absolute right"><button><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>Add Programs</button></a>            
             </div>  
             <ul class="probootstrap-owl-navigation absolute right">
               <li><a href="#" class="probootstrap-owl-prev"><i class="icon-chevron-left"></i></a></li>
