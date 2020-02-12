f307e7eba091411079b0840d33ee19b474c7e3c7
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-02-12 06:35:56 -0700

    adding more content

--- web/week05/assignment/newLW/jsFiles/loginPage.js
+++ web/week05/assignment/newLW/jsFiles/loginPage.js
@@ -1,18 +1,27 @@
 function login(value){
     if( value == "login"){
-        document.getElementById("login").style.visibility = visibility;
-        document.getElementById("signUp").style.visibility = hidden;
-        document.getElementById("trainerSignUp").style.visibility = hidden;
+        document.getElementById("login").style.display = block;
+        document.getElementById("signUp").style.display = none;
+        document.getElementById("trainerSignUp").style.display = none;
+        // document.getElementById("login").style.visibility = visibility;
+        // document.getElementById("signUp").style.visibility = hidden;
+        // document.getElementById("trainerSignUp").style.visibility = hidden;
     }
     if (value == "signUp"){
-        document.getElementById("login").style.visibility = hidden;
-        document.getElementById("signUp").style.visibility =visibility;
-        document.getElementById("trainerSignUp").style.visibility = hidden;
+        document.getElementById("login").style.display = "none";
+        document.getElementById("signUp").style.display = "block";
+        document.getElementById("trainerSignUp").style.display = "none";
+        // document.getElementById("login").style.visibility = hidden;
+        // document.getElementById("signUp").style.visibility =visibility;
+        // document.getElementById("trainerSignUp").style.visibility = hidden;
     }
     if (value == "trainerSignUp"){
-        document.getElementById("login").style.visibility = hidden;
-        document.getElementById("signUp").style.visibility = hidden;
-        document.getElementById("trainerSignUp").style.visibility = visibility;
+        document.getElementById("login").style.display = none;
+        document.getElementById("signUp").style.display = none;
+        document.getElementById("trainerSignUp").style.display = block;
+        // document.getElementById("login").style.visibility = hidden;
+        // document.getElementById("signUp").style.visibility = hidden;
+        // document.getElementById("trainerSignUp").style.visibility = visibility;
     }
-    
+
 }
