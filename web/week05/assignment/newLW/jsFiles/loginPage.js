692bbb526d528242568438d67e369456d6427206
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-02-12 13:20:37 -0700

    editing loginPageLW.php

--- web/week05/assignment/newLW/jsFiles/loginPage.js
+++ web/week05/assignment/newLW/jsFiles/loginPage.js
@@ -1,8 +1,8 @@
 function login(value){
     if( value == 1){
-        document.getElementById("login").style.display = block;
-        document.getElementById("signUp").style.display = none;
-        document.getElementById("trainerSignUp").style.display = none;
+        document.getElementById("login").style.display = "block";
+        document.getElementById("signUp").style.display = "none";
+        document.getElementById("trainerSignUp").style.display = "none";
         // document.getElementById("login").style.visibility = visibility;
         // document.getElementById("signUp").style.visibility = hidden;
         // document.getElementById("trainerSignUp").style.visibility = hidden;
@@ -16,9 +16,9 @@ function login(value){
         // document.getElementById("trainerSignUp").style.visibility = hidden;
     }
     if (value == 3){
-        document.getElementById("login").style.display = none;
-        document.getElementById("signUp").style.display = none;
-        document.getElementById("trainerSignUp").style.display = block;
+        document.getElementById("login").style.display = "none";
+        document.getElementById("signUp").style.display = "none";
+        document.getElementById("trainerSignUp").style.display = "block";
         // document.getElementById("login").style.visibility = hidden;
         // document.getElementById("signUp").style.visibility = hidden;
         // document.getElementById("trainerSignUp").style.visibility = visibility;
