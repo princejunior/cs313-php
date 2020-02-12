ba2ce9c21be593eceec98f8def974b40d3570910
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-02-12 13:10:21 -0700

    editing loginPageLW.php

--- web/week05/assignment/newLW/jsFiles/loginPage.js
+++ web/week05/assignment/newLW/jsFiles/loginPage.js
@@ -1,5 +1,5 @@
 function login(value){
-    if( value == "login"){
+    if( value == 1){
         document.getElementById("login").style.display = block;
         document.getElementById("signUp").style.display = none;
         document.getElementById("trainerSignUp").style.display = none;
@@ -7,7 +7,7 @@ function login(value){
         // document.getElementById("signUp").style.visibility = hidden;
         // document.getElementById("trainerSignUp").style.visibility = hidden;
     }
-    if (value == "signUp"){
+    if (value == 2){
         document.getElementById("login").style.display = "none";
         document.getElementById("signUp").style.display = "block";
         document.getElementById("trainerSignUp").style.display = "none";
@@ -15,7 +15,7 @@ function login(value){
         // document.getElementById("signUp").style.visibility =visibility;
         // document.getElementById("trainerSignUp").style.visibility = hidden;
     }
-    if (value == "trainerSignUp"){
+    if (value == 3){
         document.getElementById("login").style.display = none;
         document.getElementById("signUp").style.display = none;
         document.getElementById("trainerSignUp").style.display = block;
