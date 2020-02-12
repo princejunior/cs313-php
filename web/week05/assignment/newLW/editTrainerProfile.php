d8317516a43cba303891487132173f8fd2584ab0
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-02-12 03:52:59 -0700

    added textarea to editTrainerProfile.css

--- web/week05/assignment/newLW/editTrainerProfile.php
+++ web/week05/assignment/newLW/editTrainerProfile.php
@@ -104,7 +104,6 @@
   <main>
     <section id="next-section" class="probootstrap-section">
         <div class="container">
-            <a href="" class="probootstrap-owl-navigation absolute right"><button><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>Edit Content</button></a>
             <div class="row probootstrap-gutter60 mb50">
                 <div class="col-md-6">
                     <figure><img src="<?php echo $profile_img_url;?>" lt="Free Bootstrap Template by uicookies.com" class="img-responsive"></figure>
