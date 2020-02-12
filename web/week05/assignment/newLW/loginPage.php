52280b4841b37f269ebdc9563a78d14cb6edd9e0
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-02-12 13:18:38 -0700

    editing loginPageLW.php

--- web/week05/assignment/newLW/loginPage.php
+++ web/week05/assignment/newLW/loginPage.php
@@ -80,8 +80,6 @@
             </label>
         </div>
         <form id="login" class="form-signin" action="" method="POST">
-            <!-- <img class="mb-4" src="https://polar-waters-71288.herokuapp.com/week05/assignment/photos_LW/lwLogo.png" alt="" width="72" height="72"> -->
-            <!-- <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1> -->
             <label for="inputEmail" class="sr-only">Email address</label>
                 <input type="email" id="inputEmail" class="form-control"  name="email" placeholder="Email address" required="" autofocus="">
             <label for="inputPassword" class="sr-only">Password</label>
@@ -118,7 +116,7 @@
             <label for="inputPassword" class="sr-only">Password</label>
                 <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required="">
             <div class="checkbox mb-3">
-                <label>
+                
                     <input type="checkbox" name="speciality" value="Yoga">Yoga
                     <input type="checkbox" name="speciality" value="Power Lifting">Power Lifting
                     <br>
@@ -126,7 +124,7 @@
                     <input type="checkbox" name="speciality" value="Pilates">Pilates
                     <br>
                     <input type="checkbox" name="speciality" value="Cardio">Cardio
-                </label>
+                
             </div>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
             <p class="mt-5 mb-3 text-muted">© 2019-2020</p>
