fb48000f359653bb033924024b1f9d69052d8dd9
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-02-13 14:27:41 -0700

    adding a signup page for trainers

--- web/week05/assignment/newLW/insertNewTrainers.php
+++ web/week05/assignment/newLW/insertNewTrainers.php
@@ -12,17 +12,23 @@ $db = get_db();
 try
 {
 	// insert into database
-	try{
-		$query2 = 'INSERT INTO customer (email, password) VALUES (:email, :password)';
+	// try{
+	// 	$query2 = 'INSERT INTO customer (email, password) VALUES (:email, :password)';
+	// $statement = $db->prepare($query2);
+	// $statement->bindValue(':email', $email);
+	// $statement->bindValue(':password', $password);
+	// $customerId = $db->lastInsertId("customer_id_seq");
+	// }catch (Exception $ex)
+	// {
+	// 	echo "Error with DB. Details: $ex";
+	// 	die();
+	// }
+
+	$query2 = 'INSERT INTO customer (email, password) VALUES (:email, :password)';
 	$statement = $db->prepare($query2);
 	$statement->bindValue(':email', $email);
 	$statement->bindValue(':password', $password);
 	$customerId = $db->lastInsertId("customer_id_seq");
-	}catch (Exception $ex)
-	{
-		echo "Error with DB. Details: $ex";
-		die();
-	}
 	echo $customerId;
 	$query = 'INSERT INTO trainer (customer_id, first_name, last_name, speciality) VALUES (customer_id, :first, :last, :speciality)';
 	$statement = $db->prepare($query);
