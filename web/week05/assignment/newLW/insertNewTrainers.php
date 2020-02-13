695b8727c9f098120b6095fe5260bbe35d502d45
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-02-13 15:30:34 -0700

    adding a signup page for trainers

--- web/week05/assignment/newLW/insertNewTrainers.php
+++ web/week05/assignment/newLW/insertNewTrainers.php
@@ -36,7 +36,7 @@ catch (Exception $ex)
 	echo "Error with DB. Details: $ex";
 	die();
 }
-header("Location: editTrainerProfilePage.php/?id=$userId");
+header("Location: trainerProfilePage.php/?id=$userId");
 
 die(); 
 ?>
