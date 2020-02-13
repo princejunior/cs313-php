23101b9205287d5c73b026acc7899b51b8aa5cd3
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-02-13 14:46:55 -0700

    adding a signup page for trainers

--- web/week05/assignment/newLW/insertNewTrainers.php
+++ web/week05/assignment/newLW/insertNewTrainers.php
@@ -11,33 +11,22 @@ $db = get_db();
 
 try
 {
-	// insert into database
-	// try{
-	// 	$query2 = 'INSERT INTO customer (email, password) VALUES (:email, :password)';
-	// $statement = $db->prepare($query2);
-	// $statement->bindValue(':email', $email);
-	// $statement->bindValue(':password', $password);
-	// $customerId = $db->lastInsertId("customer_id_seq");
-	// }catch (Exception $ex)
-	// {
-	// 	echo "Error with DB. Details: $ex";
-	// 	die();
-	// }
-
-	$query2 = 'INSERT INTO customer (email, password) VALUES (:email, :password) RETURNING id' ;
-	$statement = $db->prepare($query2);
-	$statement->bindValue(':email', $email);
-	$statement->bindValue(':password', $password);
+	$query1 = 'INSERT INTO customer (email, password) VALUES (:email, :password)' ;
+	$statement1 = $db->prepare($query2);
+	$statement1->bindValue(':email', $email);
+	$statement1->bindValue(':password', $password);
+	$statement1->execute();
 	$customerId = $db->lastInsertId("customer_id_seq");
+	
 	echo $customerId;
-	$query = 'INSERT INTO trainer (customer_id, first_name, last_name, speciality) VALUES (customer_id, :first, :last, :speciality)';
-	$statement = $db->prepare($query);
-	$statement->bindValue(':cusomer_id', $customerId);
-	$statement->bindValue(':first', $first);
-	$statement->bindValue(':last', $last);
-	$statement->bindValue(':speciality', $speciality);
+	$query2 = 'INSERT INTO trainer (customer_id, first_name, last_name, speciality) VALUES (customer_id, :first, :last, :speciality)';
+	$statement2 = $db->prepare($query2);
+	$statement2->bindValue(':cusomer_id', $customerId);
+	$statement2->bindValue(':first', $first);
+	$statement2->bindValue(':last', $last);
+	$statement2->bindValue(':speciality', $speciality);
 
-	$statement->execute();
+	$statement2->execute();
 	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
 	// get id of last inserted row - save in $userId
 	$userId = $db->lastInsertId("trainer_id_seq");
