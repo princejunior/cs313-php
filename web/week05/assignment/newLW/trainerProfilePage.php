ad5602afba9c3087b0aa4824c3b6ae30bedcb580
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-02-14 05:45:38 -0700

    editing login

--- web/week05/assignment/newLW/trainerProfilePage.php
+++ web/week05/assignment/newLW/trainerProfilePage.php
@@ -8,10 +8,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
+<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Live W.O.W.</title>
-    <title>uiCookies:Trainer — Free Bootstrap Theme, Free Responsive Bootstrap Website Template</title>
     <meta name="description" content="">    
     <link href="https://fonts.googleapis.com/css?family=Inconsolata|Rubik:300,400,700,900" rel="stylesheet">
     <link rel="stylesheet" href="trainer/css/styles-merged.css">
@@ -20,9 +19,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Creates a drop down menu for cart -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
   <!-- To add and clear items from booking/cart -->
   <script type="text/javascript" src="trainer/js/vendor/jquery.min.js"></script>
+  
   </head>
 <body>
 <?php require 'navBarLW.php'?>
