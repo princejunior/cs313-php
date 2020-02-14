3910abe4a47298a5956a89f789381101f520d180
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-02-14 11:13:08 -0700

    editing login

--- web/week05/assignment/newLW/trainerProfilePage.php
+++ web/week05/assignment/newLW/trainerProfilePage.php
@@ -13,9 +13,9 @@
     <title>Live W.O.W.</title>
     <meta name="description" content="">    
     <link href="https://fonts.googleapis.com/css?family=Inconsolata|Rubik:300,400,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="trainer/css/styles-merged.css">
-    <link rel="stylesheet" href="trainer/css/style.min.css">
-    <link rel="stylesheet" href="trainer/css/custom.css">
+    <link rel="stylesheet" href="../trainer/css/styles-merged.css">
+    <link rel="stylesheet" href="../trainer/css/style.min.css">
+    <link rel="stylesheet" href="../trainer/css/custom.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Creates a drop down menu for cart -->
