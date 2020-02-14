cc3c9b34f512ac3bb9bc861770631243ffd81cd7
Elijah Elliott <elijahelliott@205-185-99-18.directcom.com>
2020-02-14 05:25:49 -0700

    editing login

--- web/week05/assignment/newLW/loginUser.php
+++ web/week05/assignment/newLW/loginUser.php
@@ -14,7 +14,7 @@ try
         $id = $row['id'];
         $password2 = $row['password'];
         $customer_type = $row['customer_type'];
-        $customer_id = $_SESSION['id'];
+        $_SESSION['id'] = $customer_id;
 
     }
     if($password1 != $password2){
