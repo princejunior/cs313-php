8658b456bc2a1de279cb211c121a6ea81c2182ea
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-02-14 11:08:45 -0700

    editing login

--- web/week05/assignment/newLW/loginUser.php
+++ web/week05/assignment/newLW/loginUser.php
@@ -8,36 +8,24 @@ $db = get_db();
 
 try
 {
-    $statement1 = $db->prepare("SELECT * FROM customer WHERE email = '$email'");
-    $statement1->execute();
-    while ($row1 = $statement1->fetch(PDO::FETCH_ASSOC)){
-        $customer_id = $row1['id'];
-        $password2 = $row1['password'];
-        $customer_type = $row1['customer_type'];
-        $_SESSION['customer_type'] = $customer_type;
+    $statement = $db->prepare("SELECT * FROM customer WHERE email = '$email'");
+    $statement->execute();
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+        $id = $row['id'];
+        $password2 = $row['password'];
+        $customer_type = $row['customer_type'];
+        $_SESSION['id'] = $id;
     }
-    
-
     if($password1 != $password2){
         echo "Password does not match!";
         die(); 
     } else {
+        $_SESSION['customer_type'] = $customer_type;
         if($customer_type == 'trainer'){
-            $statement2 = $db->prepare("SELECT id FROM trainer WHERE customer_id = $customer_id");
-            $statement2->execute();
-            while ($row2 = $statement2->fetch(PDO::FETCH_ASSOC)){
-            $trainer_id = $row2['id'];
-            }
-            header("Location: trainerProfilePage.php/?id=$trainer_id");
+            header("Location: trainerProfilePage.php/?id=$id");
             die(); 
         } else {
-            $_SESSION['customer_type'] = $customer_type;
-            $statement3 = $db->prepare("SELECT id FROM client WHERE customer_id = $customer_id");
-            $statement3->execute();
-            while ($row3 = $statement3->fetch(PDO::FETCH_ASSOC)){
-                $client_id = $row3['id'];
-             }
-            header("Location: clientProfilePage.php/?id=$client_id");
+            header("Location: clientProfilePage.php/?id=$id");
             die(); 
         }  
     }
