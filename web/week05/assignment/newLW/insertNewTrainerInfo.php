571a159a663e2683acee76cfe1018a072cd609d5
Elijah Elliott <elijahelliott54@gmail.com>
2020-02-18 16:08:51 -0700

    adding insert pages

--- web/week05/assignment/newLW/insertNewTrainerInfo.php
+++ web/week05/assignment/newLW/insertNewTrainerInfo.php
@@ -6,13 +6,14 @@ $last = $_POST['last_name'];
 $about_me = $_POST['about_me'];
 $my_mission = $_POST['my_mission'];
 $my_vision = $_POST['my_vision'];
+$trainer_id = $_SESSION['trainer_id'];
 require("dbConnect.php");
 $db = get_db();
 try
 {
 	$query1 = 'INSERT INTO trainer_desscription (trainer_id, about_me, my_mission, my_vision) VALUES (:trainer_id, :about_me, :my_mission, :my_vision)' ;
     $statement1 = $db->prepare($query1);
-	$statement1->bindValue(':trainer', $_SESSION['trainer_id']);    
+	$statement1->bindValue(':trainer', $trainer_id);    
 	$statement1->bindValue(':about_me', $about_me);
 	$statement1->bindValue(':my_mission', $my_mission);
 	$statement1->bindValue(':my_vision', $my_vision);
