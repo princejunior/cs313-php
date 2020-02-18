45cb44bb4e04c4ced7a7e6032a5f68bb8b6f1c4b
Elijah Elliott <elijahelliott54@gmail.com>
2020-02-18 15:29:56 -0700

    editing editProfile pages

--- web/week05/assignment/newLW/insertNewTrainers.php
+++ web/week05/assignment/newLW/insertNewTrainers.php
@@ -7,9 +7,9 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $speciality = $_POST['speciality'];
 $customer_type = 'trainer';
+$profile_img_url = 'https://polar-waters-71288.herokuapp.com/week05/assignment/photos_LW/defaultImg.png';
 require("dbConnect.php");
 $db = get_db();
-
 try
 {
 	$query1 = 'INSERT INTO customer (email, password, customer_type) VALUES (:email, :password, :customer_type)' ;
@@ -21,18 +21,20 @@ try
 	$customerId = $db->lastInsertId("customer_id_seq");
 	
 	echo $customerId;
-	$query2 = 'INSERT INTO trainer (customer_id, first_name, last_name, speciality) VALUES (:customer_id, :first, :last, :speciality)';
+	$query2 = 'INSERT INTO trainer (customer_id, first_name, last_name, speciality,profile_img_url) VALUES (:customer_id, :first, :last, :speciality, :profile_img_url)';
 	$statement2 = $db->prepare($query2);
 	$statement2->bindValue(':customer_id', $customerId);
 	$statement2->bindValue(':first', $first);
 	$statement2->bindValue(':last', $last);
 	$statement2->bindValue(':speciality', $speciality);
+	$statement2->bindValue(':profile_img_url', $profile_img_url);
 	$statement2->execute();
-	$query3 = 'INSERT INTO client (customer_id, first_name, last_name) VALUES (:customer_id, :first, :last)';
+	$query3 = 'INSERT INTO client (customer_id, first_name, last_name, profile_img_url) VALUES (:customer_id, :first, :last, :profile_img_url)';
 	$statement3 = $db->prepare($query3);
 	$statement3->bindValue(':customer_id', $customerId);
 	$statement3->bindValue(':first', $first);
 	$statement3->bindValue(':last', $last);
+	$statement3->bindValue(':profile_img_url', $profile_img_url);
 	$statement3->execute();
 	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
 	// get id of last inserted row - save in $userId
@@ -45,7 +47,7 @@ catch (Exception $ex)
 	echo "Error with DB. Details: $ex";
 	die();
 }
-header("Location: trainerProfilePage.php/?customer_id=$customerId");
+header("Location: insertTrainerInfo.php/?customer_id=$customerId");
 
 die(); 
 ?>
