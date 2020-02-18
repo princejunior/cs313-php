ee5056c27324025cc818758b6f80016cbfa21a61
Elijah Elliott <elijahelliott54@gmail.com>
2020-02-17 20:35:19 -0700

    adding paige info

--- web/week05/assignment/newLW/database.sql
+++ web/week05/assignment/newLW/database.sql
@@ -177,7 +177,7 @@ INSERT INTO trainer ( customer_id
                  VALUES (2
                         ,'Paige'
                         , 'Bartlett'
-                        , 'https://polar-waters-71288.herokuapp.com/week05/assignment/trainerProfilePicture/paige_bartlett_2_horiz.jpeg'
+                        , 'https://polar-waters-71288.herokuapp.com/week05/assignment/trainerProfilePicture/paige_bartlett_2_horiz.jpg'
                         ,'HyperTrophy'
                         );
 INSERT INTO trainer ( customer_id
