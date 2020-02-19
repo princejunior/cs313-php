3ad414badb9a3e2d287794b9e8b2dbeecadad18b
Elijah Elliott <elijahelliott54@gmail.com>
2020-02-18 23:56:11 -0700

    adding insert pages

--- web/week05/assignment/newLW/saveTrainerChanges.php
+++ web/week05/assignment/newLW/saveTrainerChanges.php
@@ -1,23 +1,26 @@
 <?php
+    session_start();
+    require "dbConnect.php";
+    $db = get_db();
     $about_me_1 = htmlspecialchars($_POST["about_me"]);
 	$my_mission_1 = htmlspecialchars($_POST['my_mission']);
 	$my_vision_1 = htmlspecialchars($_POST['my_vision']);
 	$customer_id = htmlspecialchars($_GET['customer_id']);
     try{
-    $statement = $db->prepare("SELECT * FROM trainer_description WHERE trainer_id = 
-    (Select id FROM trainer WHERE customer_id = $customer_id)");
-    $statement->execute();
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-    {
-        $trainer_id = $row['trainer_id'];
-        $about_me_2 = $row['about_me'];
-        $my_mission_2 = $row['my_mission'];
-        $my_vision_2 = $row['my_vision'];
-    }
-    $query1 = "UPDATE trainer_description SET about_me='$about_me_1', my_mission='$my_mission_1', my_vision='$my_vision_1' WHERE client_id = $trainer_id";
-    $update1 = $db->prepare($query1);
-	// $update1->bindValue(':email', $email);
-    $update1->execute();
+        $statement = $db->prepare("SELECT * FROM trainer_description WHERE trainer_id = 
+            (Select id FROM trainer WHERE customer_id = $customer_id)");
+        $statement->execute();
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+            $trainer_id = $row['trainer_id'];
+            $about_me_2 = $row['about_me'];
+            $my_mission_2 = $row['my_mission'];
+            $my_vision_2 = $row['my_vision'];
+        }
+        $query1 = "UPDATE trainer_description SET about_me='$about_me_1', my_mission='$my_mission_1', my_vision='$my_vision_1' WHERE client_id = $trainer_id";
+        $update1 = $db->prepare($query1);
+	    // $update1->bindValue(':email', $email);
+        $update1->execute();
     
     // if ($about_me_1 != $about_me_2){
     //     $query1 = "UPDATE trainer_description SET about_me='$about_me_1'  WHERE client_id = $trainer_id";
