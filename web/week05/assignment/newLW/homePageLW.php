d44957aa0a15a57752a12ea3fbb2bb1e7bd8b414
Elijah Elliott <elijahelliott54@gmail.com>
2020-02-19 01:15:30 -0700

    add customer_id to all pages and in url

--- web/week05/assignment/newLW/homePageLW.php
+++ web/week05/assignment/newLW/homePageLW.php
@@ -66,7 +66,7 @@
                                     Description</p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
-                                        <a href='profilePageLW.php?customer_id=<?php echo $customer_id?> trainer_id=<?php echo $trainer_id;?>'><button class="btn btn-primary" value="<?php echo $id?>">View Profile</button></a>
+                                        <a href='../profilePageLW.php?customer_id=<?php echo $customer_id?> trainer_id=<?php echo $trainer_id;?>'><button class="btn btn-primary" value="<?php echo $id?>">View Profile</button></a>
                                         <!-- <button class="btn btn-primary" value="<?php echo $id;?>">Add to Book</button> -->
                                     </div>
                                 </div> 
